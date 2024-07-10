aaf87917cf7557e5ce6410352cba2b03d817dc33
Dikshant <121669947+pingu-73@users.noreply.github.com>
2024-07-10 20:29:07 +0530

    Confine to the crate project structure (#1)
    * defining project structure
    
    * removed wrapper.cpp config from build.rs
    
    * added tests for init + added bindings for fn from hpx/init.hpp as per public api.
    
    * added License
    
    * removed main.rs and added lib.rs lib.rs will also contains docs for the crate
    
    * updated README.md
    
    * corrected docs to pass the doc test
    
    * removed unnecessary comments
    
    * removed linking to hpx_iostreams and hpx dylib
    
    ---------
    
    Signed-off-by: Dikshant <dikshant.073@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hpx-rs
+Rust bindings to HPX, C++ Standard Library for Concurrency and
+Parallelism.
+
+## Rust version requirements
+hpx-rs works with stable Rust.
+
+## Version of hpx
+Currently this library requires hpx-1.10(or newer). The user is required
+to pre-install hpx and set the pkg-config to point to "*.pc" files of
+hpx. The hpx-sys crate will then link to the hpx.
---
+++ hpx-sys/src/lib.rs
@@ -0,0 +1,60 @@
+#![doc(html_root_url = "https://github.com/STEllAR-GROUP/hpx-rs")]
+#![allow(bad_style, non_camel_case_types, unused_extern_crates)]
+#![allow(dead_code, unused_imports)]
+
+#[cxx::bridge]
+pub mod ffi {
+    unsafe extern "C++" {
+        include!("hpx-sys/include/wrapper.h");
+
+        //fn start() -> i32;
+        unsafe fn init(
+            func: unsafe fn(i32, *mut *mut c_char) -> i32,
+            argc: i32,
+            argv: *mut *mut c_char,
+        ) -> i32;
+
+        fn finalize() -> i32;
+        fn finalize_with_timeout(shutdown_timeout: f64, localwait: f64) -> i32;
+        fn terminate();
+        fn disconnect() -> i32;
+        fn disconnect_with_timeout(shutdown_timeout: f64, localwait: f64) -> i32;
+        //fn stop() -> i32;
+    }
+}
+
+// ================================================================================================
+// Tests (to be shifted to systests crate within hpx-rs workspace)
+// ================================================================================================
+#[cfg(test)]
+mod tests {
+    use super::ffi;
+    use serial_test::serial;
+    use std::ffi::CString;
+    use std::os::raw::c_char;
+    use std::thread;
+    use std::time::Duration;
+
+    fn create_c_args(args: &[&str]) -> (i32, Vec<*mut c_char>) {
+        let c_args: Vec<CString> = args.iter().map(|s| CString::new(*s).unwrap()).collect();
+        let ptrs: Vec<*mut c_char> = c_args.iter().map(|s| s.as_ptr() as *mut c_char).collect();
+        (ptrs.len() as i32, ptrs)
+    }
+
+    #[test]
+    fn test_init_finalize() {
+        let (argc, mut argv) = create_c_args(&["testing", "arg1", "arg2"]);
+
+        let dummy_main = |_argc: i32, _argv: *mut *mut c_char| -> i32 {
+            println!("Dummy fn called");
+            // to exit hpx::init you are required to shutdown hpx runtime
+            ffi::finalize();
+            0
+        };
+
+        unsafe {
+            let result = ffi::init(dummy_main, argc, argv.as_mut_ptr());
+            assert_eq!(result, 0);
+        }
+    }
+}
