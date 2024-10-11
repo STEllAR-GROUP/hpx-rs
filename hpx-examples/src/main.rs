1d71bbca7d6eb4f597afffd63852a6147fcaccac
Connor-GH <connor-gh@outlook.com>
2024-10-10 19:44:44 -0500

    Fix rustfmt issues from CI

--- hpx-examples/src/main.rs
+++ hpx-examples/src/main.rs
@@ -1,12 +1,11 @@
 #![feature(vec_into_raw_parts)]
 #![feature(random)]
 use core::array::from_fn;
-use std::{process, random, ffi::c_char, env};
+use std::{env, ffi::c_char, process, random};
 
 use hpx_sys as hpx;
 
 fn hpx_main(argc: i32, argv: *mut *mut c_char) -> i32 {
-
     let numbers: &[i32; 16384] = &from_fn(|_| random::random::<i32>());
     let list: &mut Vec<i32> = &mut Vec::<i32>::from(numbers);
     println!("{:#?}", list);
@@ -18,15 +17,14 @@ fn hpx_main(argc: i32, argv: *mut *mut c_char) -> i32 {
 }
 fn main() {
     let args = env::args();
-    let (argc, argv) = hpx::create_c_args(&args
-        .collect::<Vec<String>>()
-        .iter()
-        .map(|s| s.as_str())
-        .collect::<Vec<&str>>()
+    let (argc, argv) = hpx::create_c_args(
+        &args
+            .collect::<Vec<String>>()
+            .iter()
+            .map(|s| s.as_str())
+            .collect::<Vec<&str>>(),
     );
     unsafe {
-    process::exit(hpx::init(hpx_main, argc, argv
-            .into_raw_parts()
-            .0));
+        process::exit(hpx::init(hpx_main, argc, argv.into_raw_parts().0));
     }
 }
